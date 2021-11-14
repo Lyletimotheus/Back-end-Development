227fd90fc88b420efa2eb988f6a772b6ff7d1a78
Lyle Timotheus <lyle6494@gmail.com>
2021-11-14 18:06:53 +0200

    Global objects in node

---
+++ Node Refresher/notes.js
@@ -0,0 +1,14 @@
+// Timestamp: 00:21:56
+
+// ###########################
+// #### Global Objects #######
+// ###########################
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+console.log();
+// Node uses a global object
+globalThis.setInterval(); // This is the same as the above function.
