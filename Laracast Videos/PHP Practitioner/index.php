a6736bba218eed5b08965a2230753f969e0a2c8c
Lyle Timotheus <lyle6494@gmail.com>
2021-11-28 22:25:09 +0200

    Seperation of concerns

--- Laracast Videos/PHP Practitioner/index.php
+++ Laracast Videos/PHP Practitioner/index.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Refresher</title>
-    <style>
-        header{
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>
-            <?= 'Hello, '. htmlspecialchars($_GET['name']); ?>
-        </h1>
-    </header>
-</body>
-</html>
+<?php 
+    $greeting = "Hello World";
+
+    require 'index.view.php';
+
