d5335d37580a73da968f329d7f449b0c5cdbc542
Lyle Timotheus <lyle6494@gmail.com>
2021-11-30 20:15:46 +0200

    Classes

--- Laracast Videos/PHP Practitioner/index.php
+++ Laracast Videos/PHP Practitioner/index.php
@@ -2,7 +2,7 @@
     require 'functions.php';
 
     // $names = ['Jeff', 'Lyle', 'Carlos'];
-    $animals = ['Cat', 'Bat', 'Snake', 'Rat','Dog'];
+    // $animals = ['Cat', 'Bat', 'Snake', 'Rat','Dog'];
 
     // $person = [
     //     'name' => 'Lyle Timotheus',
@@ -36,8 +36,50 @@
     // dd($animals);
 
     // Calling the age function to determine age 
-    echo age(3);
+    // echo age(3);
 
+##################
+#### CLASSES  ####
+##################
+
+    // Todo Application
+    // Todo, Comment, User
+    class Task {
+        // Look at your database to determine what you might need for a task
+        // We include two protected properties.
+        // We would not add the completed property to the constructor as every task we add at the beginning will be incomplete (set to false).
+        public $description;
+        public $completed = false;
+
+        public function __construct($description)
+        {
+            // Automatically triggered on instantiation (new keyword)
+            $this->description = $description;
+        }
+
+        public function complete() {
+            $this->completed = true;
+        }
+
+        public function isComplete() {
+            return $this-> completed;
+        }
+    }
+
+    // $task = new Task('Go to the store');
+    // $task->complete();
+    // var_dump($task->isComplete());
+    // var_dump($task);
+
+    // Array of tasks 
+    $tasks = [
+        new Task('Go to the store'),
+        new Task('Finish my homework'),
+        new Task('Clean my bedroom')
+    ];
+
+    $tasks[0]->complete();
+    // dd($tasks);
     require 'index.view.php';
    
 
--- Laracast Videos/PHP Practitioner/index.view.php
+++ Laracast Videos/PHP Practitioner/index.view.php
@@ -63,5 +63,20 @@
                 <?php endif ; ?>
             </li>
         </ul> -->
+
+        <ul>
+            <?php foreach ($tasks as $task) : ?>
+                <li>
+                    <?php if($task->completed) : ?>
+                        <strike>
+                    <?php endif ?>
+
+                    <?= $task->description ?>
+                    <?php if($task->completed) : ?>
+                        </strike>
+                        <?php endif ?>
+                </li>
+            <?php endforeach ;?>
+        </ul>
 </body>
 </html>
